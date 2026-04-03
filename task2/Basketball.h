10ff12b640ecc7dd4e1381934f8a11f03a026672
Yarmolik Anastasiya <yormolik.anastasiya@gmail.com>
2026-04-03 09:12:24 +0300

    feat: add task2 - basketball penalty tracker

---
+++ task2/Basketball.h
@@ -0,0 +1,14 @@
+#import <Foundation/Foundation.h>
+
+@interface Basketball : NSObject
+
+@property (nonatomic, assign) NSInteger team1Time;
+@property (nonatomic, assign) NSInteger team2Time;
+
+- (void)addPenalty:(NSInteger)minutes;
+           forTeam:(NSInteger)team;
+              type:(NSString *)type;
+- (void)printCurrentScore;
+- (void)printFinalResult;
+
+@end
